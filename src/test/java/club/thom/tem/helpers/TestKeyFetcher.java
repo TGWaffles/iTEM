c8dc2589f98063fd2b63a0261801ee99bbfb3282
Orenico10 <oren.lev987@gmail.com>
2022-01-19 01:29:40 +0200

    added empty test+resources

---
+++ src/test/java/club/thom/tem/helpers/TestKeyFetcher.java
@@ -0,0 +1,15 @@
+package club.thom.tem.helpers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PowerMockIgnore("TEM#sendMessage")
+public class TestKeyFetcher {
+    @Test
+    public void TestKeyFetcher(){
+
+    }
+}
