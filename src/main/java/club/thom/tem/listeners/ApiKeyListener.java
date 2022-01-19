56ff169b930abb955712c1b5918343d0c71cd0ab
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2022-01-19 19:14:53 +0000

    wait for rate-limit friendly request
    when listening for api key, doesn't say in chat until it's actually set.

--- src/main/java/club/thom/tem/listeners/ApiKeyListener.java
+++ src/main/java/club/thom/tem/listeners/ApiKeyListener.java
@@ -7,16 +7,29 @@ import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ApiKeyListener {
+    private static final Logger logger = LoggerFactory.getLogger(ApiKeyListener.class);
+
     @SubscribeEvent(receiveCanceled = true, priority = EventPriority.HIGH)
     public void onChat(ClientChatReceivedEvent event) {
-        String unformatted = EnumChatFormatting.getTextWithoutFormattingCodes(event.message.getFormattedText());
-        if (unformatted.startsWith("Your new API key is ") && event.message.getSiblings().size() >= 1) {
-            String apiKey = event.message.getSiblings().get(0).getChatStyle().getChatClickEvent().getValue();
-            TEMConfig.setHypixelKey(apiKey);
-            TEM.forceSaveConfig();
-            TEM.sendMessage(new ChatComponentText("API key set to " + apiKey + "!"));
-        }
+        new Thread(() -> {
+            String unformatted = EnumChatFormatting.getTextWithoutFormattingCodes(event.message.getFormattedText());
+            if (unformatted.startsWith("Your new API key is ") && event.message.getSiblings().size() >= 1) {
+                logger.debug("Found API key message in chat!");
+                String apiKey = event.message.getSiblings().get(0).getChatStyle().getChatClickEvent().getValue();
+                logger.debug("Setting API key to: {}", apiKey);
+                try {
+                    TEMConfig.setHypixelKey(apiKey).join();
+                } catch (InterruptedException e) {
+                    logger.error("setHypixelKey was interrupted.");
+                    return;
+                }
+                logger.debug("API key set to: {} successfully", apiKey);
+                TEM.sendMessage(new ChatComponentText("API key set to " + apiKey + "!"));
+            }
+        }).start();
     }
 }
