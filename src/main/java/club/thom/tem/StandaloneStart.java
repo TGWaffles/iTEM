ecffbd3670e8022ace690b5ed6c5bc3e52b6a53e
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2022-02-08 21:14:03 +0000

    allow multiple standalone versions to share a folder

--- src/main/java/club/thom/tem/StandaloneStart.java
+++ src/main/java/club/thom/tem/StandaloneStart.java
@@ -1,19 +1,29 @@
 package club.thom.tem;
 
+import club.thom.tem.hypixel.Hypixel;
 import club.thom.tem.storage.TEMConfig;
 import gg.essential.vigilance.Vigilance;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.lang.reflect.Field;
 
 public class StandaloneStart {
+    private static final Logger logger = LogManager.getLogger(StandaloneStart.class);
 
     public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
         Field initialized = Vigilance.class.getDeclaredField("initialized");
         initialized.setAccessible(true);
         initialized.set(null, true);
-
+        if (args.length < 2) {
+            logger.error("You must include the uuid and API key as an argument to this program!\nUsage: java -jar TEMStandalone.jar <uuid> <api-key>");
+            Runtime.getRuntime().exit(1);
+        }
+        String uuid = args[0];
+        String apiKey = args[1];
         TEMConfig.saveFolder = "config/";
-        TEM.main(args);
+        TEMConfig.fileName = apiKey + ".toml";
+        TEM.main(uuid, apiKey);
     }
 
 }
