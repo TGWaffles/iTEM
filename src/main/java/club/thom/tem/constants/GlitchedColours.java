657d06393c18710ed5501a2816b3c0c587093b6e
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2022-05-07 19:51:22 +0100

    define default colours for wither armour

---
+++ src/main/java/club/thom/tem/constants/GlitchedColours.java
@@ -0,0 +1,26 @@
+package club.thom.tem.constants;
+
+import com.google.common.collect.ImmutableMap;
+
+public class GlitchedColours {
+    public static final ImmutableMap<String, String> CHESTPLATE_COLOURS = ImmutableMap.of(
+            "E7413C", "POWER_WITHER_CHESTPLATE",
+            "45413C", "TANK_WITHER_CHESTPLATE",
+            "4A14B7", "SPEED_WITHER_CHESTPLATE",
+            "1793C4", "WISE_WITHER_CHESTPLATE"
+    );
+
+    public static final ImmutableMap<String, String> LEGGINGS_COLOURS = ImmutableMap.of(
+            "E75C3C", "POWER_WITHER_LEGGINGS",
+            "65605A", "TANK_WITHER_LEGGINGS",
+            "5D2FB9", "SPEED_WITHER_LEGGINGS",
+            "17A8C4", "WISE_WITHER_LEGGINGS"
+    );
+
+    public static final ImmutableMap<String, String> BOOT_COLOURS = ImmutableMap.of(
+            "E76E3C", "POWER_WITHER_BOOTS",
+            "88837E", "TANK_WITHER_BOOTS",
+            "8969C8", "SPEED_WITHER_BOOTS",
+            "1CD4E4", "WISE_WITHER_BOOTS"
+    );
+}
